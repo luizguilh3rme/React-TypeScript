8bb7483d066afcd783b283988346214f2fc85cb1
Luiz Guilherme Muniz barreto <166665419+luizguilh3rme@users.noreply.github.com>
2025-09-30 22:07:57 -0300

    att

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,17 +1,78 @@
-import {Header} from './components/header'
-import {Aluno} from './components/aluno'
-import {Footer} from './components/footer'
+import {useState}from 'react'
+
+// Interface para unir  o nome e idade
+interface InfoAlunoProps{
+  nome: string;
+  idade: string;
+}
 
 export default function App(){
+  // Const para mostrar nome e idade, a const roxa guarda o valor e a const verde atualiza o valor
+  const [input, setInput] = useState("");
+  const [idade, setIdade] = useState("");
+  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>()
+
+  // Const para executar o contador
+  const [contador, setContador] =  useState(0);
+
+
+  //Função para executar e mostrar o nome e idade
+  function mostrarAluno(){
+    setInfoAluno({
+      nome: input,
+      idade: idade,
+    })
+  }
+
+
+  //Funções para executar o contador a partir do método onclick
+  function adicionar(){
+    setContador(valorAtual => valorAtual +1)
+  }
+
+  function diminuir(){
+    if(contador === 0){ //impede que o contador va para os números negativos
+      return;
+    }
+    setContador(valorAtual => valorAtual -1)
+  }
+
+
   return(
     <div>
+      {/* Código para mostrar nome e idade */}
+      <h1>Conhecendo useSate</h1>
 
-      <Header title='Aprendendo React!'/>
+      <input type="text"
+      placeholder="Digite seu nome"
+      value={input}
+      onChange={ (e) => setInput(e.target.value)} />
 
-      <Aluno nome="Luiz Guilherme" idade={20} />
-      <Aluno nome="Luiz Gustavo" idade={21} />
+      <br />
+      <br />
 
-      <Footer/>
+      <input type="text"
+      placeholder='Digite sua idade'
+      value={idade}
+      onChange={ (e) => setIdade (e.target.value)} />
+
+      <br />
+      <br />
+
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+
+      <hr />
+
+      <h3>Bem vindo: {infoAluno?.nome}</h3>
+      <h4>Idade: {infoAluno?.idade}</h4>
+
+      <hr />
+      <br />
+
+
+      {/* Código do contador */}
+      <h1>Contador com useState</h1>
+      <button onClick={adicionar}>+</button> {contador} <button onClick={diminuir}>-</button>
 
     </div>
   )
--- primeiro/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer(){
-    return(
-        <footer>
-            <h4>Todos os direitos reservados - @curso</h4>
-        </footer>
-    )
-}
